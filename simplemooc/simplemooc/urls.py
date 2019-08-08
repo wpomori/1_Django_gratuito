eae2dcc26ab86954bbecf05147c37af5a23c52e7
Wellington Pine Omori <wpomori@gmail.com>
2019-08-07 23:53:47 -0300

    Project directory

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^', include('simplemooc.core.urls', namespace='core')),
+    url(r'^', include('simplemooc.core.urls', namespace='core')),
     url(r'^admin/', include(admin.site.urls)),
 )
