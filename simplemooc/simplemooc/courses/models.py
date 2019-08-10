0bc6bc5f5860bb86611cf2ff52a37840733396f6
Wellington Pine Omori <wpomori@gmail.com>
2019-08-10 08:42:05 -0300

    Add custom Manager in Models

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class CourseManager(models.Manager):
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) | \
+            models.Q(description__icontains=query)
+        )
+
+
 class Course(models.Model):
     name = models.CharField(
         'Nome',
@@ -30,3 +38,4 @@ class Course(models.Model):
         'Atualizado em',
         auto_now=True
     )
+    objects = CourseManager()
