6920aae0f911f1e2593ea3fb29c0a367b246f68b
Wellington Pine Omori <wpomori@gmail.com>
2019-08-14 02:16:25 -0300

    Add media files and for/if in template file

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Course
 
 
 def index(request):
+    courses = Course.objects.all()
     template_name = 'courses/index.html'
-    return render(request, template_name)
+    context = {
+        'courses': courses
+    }
+    return render(request, template_name, context)
