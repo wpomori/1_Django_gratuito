489d4a663c13c7cdf1703d63c907a156edcb4b57
Wellington Pine Omori <wellington.omori@neoprospecta.com>
2020-05-01 13:10:43 -0300

    Added login variables for use in login page

--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -136,3 +136,8 @@ EMAIL_HOST_PASSWORD = 'senha'
 EMAIL_PORT = 587
 
 CONTACT_EMAIL = 'contato@simplemooc.com'
+
+# Auth
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'core:home'
+LOGOUT_URL = 'accounts:logout'
