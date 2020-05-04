5ee78e484a39c800a7b82797b5ad4fd07b572035
Wellington Pine Omori <wellington.omori@neoprospecta.com>
2020-05-04 12:50:50 -0300

    Mapped url cadastra-se to register function

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.views import LoginView
-from django.urls import path
+from django.urls import path, include
 from . import views
+from simplemooc.accounts import views
 
 app_name = "accounts"
 urlpatterns = [
+    #path('conta/', include(('simplemooc.accounts.urls'), namespace='accounts')),
     path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('cadastra-se/', views.register, name='register'),
 ]
