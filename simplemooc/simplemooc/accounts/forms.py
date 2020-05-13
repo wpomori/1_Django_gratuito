2fef1eb40629d1068ad4a72dc10a1188e6f562da
Wellington Omori <wpomori@gmail.com>
2020-05-13 09:02:29 -0300

    Create display to custom User

--- simplemooc/simplemooc/accounts/forms.py
+++ simplemooc/simplemooc/accounts/forms.py
@@ -1,21 +1,38 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
-class RegisterForm(UserCreationForm):
+User = get_user_model()
 
-    email = forms.EmailField(label='E-mail')
+class RegisterForm(forms.ModelForm):
 
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('Já existe usuário com este E-mail')
-        return email
-    
+    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmação de Senha', widget=forms.PasswordInput)
+
+    def cleaned_password(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('A confirmação não está correta')
+        return password2
+
+
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
+        user.set_password(self.cleaned_data['password1'])
+
         if commit:
             user.save()
         return user
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class EditAccountForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'name']
