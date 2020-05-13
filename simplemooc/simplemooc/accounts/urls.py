4fe434b17502a38a0089d2277af637c36f432f13
Wellington Omori <wpomori@gmail.com>
2020-05-13 05:08:30 -0300

    Addition of LogoutView function for user to disconnect from the system

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -1,11 +1,12 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from . import views
 from simplemooc.accounts import views
 
 app_name = "accounts"
 urlpatterns = [
-    #path('conta/', include(('simplemooc.accounts.urls'), namespace='accounts')),
+    path('', views.dashboard, name='dashboard'),
     path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('sair/', LogoutView.as_view(next_page='core:home'), name='logout'),
     path('cadastra-se/', views.register, name='register'),
 ]
