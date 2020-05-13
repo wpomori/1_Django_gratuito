30c99f66ab4926fb8716ae6785147880cca3484c
Wellington Omori <wpomori@gmail.com>
2020-05-13 09:03:29 -0300

    Make HTML file to editing account

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('sair/', LogoutView.as_view(next_page='core:home'), name='logout'),
     path('cadastra-se/', views.register, name='register'),
+    path('editar/', views.edit, name='edit'),
+    path('editar-senha/', views.edit_password, name='edit_password'),
 ]
