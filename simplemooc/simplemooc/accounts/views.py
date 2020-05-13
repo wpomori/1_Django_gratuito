89a34d93ab74f811c227b097a97ffb482fb8091c
Wellington Omori <wpomori@gmail.com>
2020-05-13 05:05:50 -0300

    Added step to veryfy that username and password are valid

--- simplemooc/simplemooc/accounts/views.py
+++ simplemooc/simplemooc/accounts/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 from .forms import RegisterForm
 
+@login_required
+def dashboard(request):
+    template_name = 'accounts/dashboard.html'
+    return render(request, template_name)
 
 def register(request):
     template_name = 'accounts/register.html'
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+            user = authenticate(
+                username=user.username, password=form.cleaned_data['password1']
+            )
+            login(request, user)
+            return redirect('core:home')
     else:
         form = RegisterForm()
     context = {
