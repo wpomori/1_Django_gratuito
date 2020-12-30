3a9998a6bb7ceeb3e0fecd972a6cc0fe5ecc1f6f
Wellington Omori <wpomori@gmail.com>
2020-12-30 06:03:08 -0300

    List user fields in admin application

--- simplemooc/simplemooc/accounts/admin.py
+++ simplemooc/simplemooc/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'name', 'email', 'is_activate', 'is_staff', 'date_joined')
+
+
+admin.site.register(User, UserAdmin)
